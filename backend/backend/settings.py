0ae121145f559b174a69b17b337833df69a8134d
Pravalika-Batchu <121426817+Pravalika-Batchu@users.noreply.github.com>
2026-04-03 13:08:37 +0530

    Modified backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-fallback-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-
+#hosts for deployment
 ALLOWED_HOSTS = ['*']
 
 # JWT settings
